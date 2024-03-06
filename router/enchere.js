99b519b0e06fda19cc6fd0a5ab5081ab56b49d28
Fabrice243 <152931886+Fabrice243@users.noreply.github.com>
2024-03-05 22:38:36 -0500

    Soumisions Fabrice

---
+++ router/enchere.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { body, param } from 'express-validator';
+import { getAllEncheres, getEnchereById, createEnchere, updateEnchere, deleteEnchere } from "../controller/enchere.js";
+
+const enchere = Router();
+
+const validateEnchere = [
+  body('titre').notEmpty().withMessage('Le titre est obligatoire'),
+  body('description').notEmpty().withMessage('La description est obligatoire'),
+  body('prix_depart').isNumeric().withMessage('Le prix de départ doit être un nombre'),
+  body('date_debut').isISO8601().toDate().withMessage('La date de début doit être une date au format ISO8601'),
+  body('date_fin').isISO8601().toDate().withMessage('La date de fin doit être une date au format ISO8601'),
+  body('id_utilisateur').notEmpty().withMessage('L\'ID de l\'utilisateur est obligatoire'),
+];
+
+enchere
+  .get("/", getAllEncheres)
+  .get("/:id", param('id').isMongoId().withMessage('L\'ID doit être un ObjectId valide'), getEnchereById)
+  .post("/", validateEnchere, createEnchere)
+  .put("/:id", [
+    param('id').isMongoId().withMessage('L\'ID doit être un ObjectId valide'),
+    ...validateEnchere,
+  ], updateEnchere)
+  .delete("/:id", param('id').isMongoId().withMessage('L\'ID doit être un ObjectId valide'), deleteEnchere);
+
+export default enchere;
